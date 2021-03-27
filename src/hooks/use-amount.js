9e21a534cf488e00a04b0150327a67eabb57aca3
Nomta <nomta@mail.ru>
2021-03-27 15:51:22 +0300

    update

--- src/hooks/use-amount.js
+++ src/hooks/use-amount.js
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 
-export default function useAmount(initialAmount) {
+export default function useAmount(initialAmount = 0) {
   const [amount, setAmount] = useState(initialAmount);
 
-  const decrement = () => setAmount(amount > 0 ? amount - 1 : 0);
+  const decrement = () => setAmount(amount && amount - 1);
   const increment = () => setAmount(amount + 1);
 
   return { decrement, increment, amount };
