d3cc46433b76f359c909dc01c0395846e7b7f75e
Nomta <nomta@mail.ru>
2021-03-27 18:57:08 +0300

    Rate

--- src/components/product.js
+++ src/components/product.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Counter from './counter';
+import Rate from './rate';
 
 import style from './product.module.css';
 
 function Product(props) {
+  const [rate, setRate] = useState(0)
+  const handleClick = (e) => setRate(e.currentTarget.dataset.index)
+  
   return (
     <div className={style.card}>
-      <h3>{props.product.name}</h3>
+      <h3 className={style.title}>
+        {props.product.name}
+        <Rate value={rate} onClick={handleClick}/>
+      </h3>
       <div className="flex-between">
-        <span className={style.price}>${props.product.price}</span>
+        <span className={style.price}>
+          ${props.product.price}
+        </span>
         <Counter/>
       </div>
     </div>
