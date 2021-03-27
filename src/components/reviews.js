b2efdb4ecf5780b55d13f92380cada48f175450e
Valentina Kirsanova <kirsanova.va@mobilcard.ru>
2021-03-27 19:18:21 +0300

    styles for reviews

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -1,17 +1,19 @@
 import Rate from './rate';
 
+import style from './reviews.module.css';
+
 export default function Reviews(props) {
   const { reviews } = props;
   const renderedReviews = reviews
     .map(({ id, user, text, rating }) => (
       <div
-        className="review"
+        className={style.review}
         key={id}
       >
-        <div className='review__user'>
+        <div className={style.review__user}>
           {user}
         </div>
-        <div className='review__text'>
+        <div className={style.review__text}>
           {text}
         </div>
         <Rate value={rating} />
@@ -20,7 +22,10 @@ export default function Reviews(props) {
 
   return (
     <div className="reviews">
-      {renderedReviews}
+      <h3>Reviews</h3>
+      <div className='reviews__body'>
+        {renderedReviews}
+      </div>
     </div>
   );
 }
