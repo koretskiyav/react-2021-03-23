3a5c4710c6793264b3c1f8617f77b08df05192c4
Valentina Kirsanova <kirsanova.va@mobilcard.ru>
2021-03-27 18:57:28 +0300

    fixed rate component

--- src/components/rate.js
+++ src/components/rate.js
@@ -1,16 +1,19 @@
 import { ReactComponent as Star } from '../icons/star.svg';
-import { ReactComponent as FilledStar } from '../icons/star.svg';
+import { ReactComponent as FilledStar } from '../icons/filledStar.svg';
 
 export default function Rate (props) {
   const { value } = props;
   const displayedRate = value < 1 ? 1 :
-    value > 5 ? 5 : value;
-  const filledStars = new Array(displayedRate);
-  const emptyStars = new Array(5 - displayedRate);
+    value > 5 ? 5 : Math.round(value);
+  const filledStars = (new Array(displayedRate))
+    .fill(0);
+  const renderEmptyStars = displayedRate < 5;
+  const emptyStars = renderEmptyStars && (new Array(5 - displayedRate))
+    .fill(0);
 
   const renderedFilledStars = filledStars
     .map((star, id) => <FilledStar key={`filled-star_${id}`} />);
-  const renderedEmptyStars = emptyStars
+  const renderedEmptyStars = renderEmptyStars && emptyStars
     .map((star, id) => <Star key={`empty-star_${id}`} />);
 
   return (
