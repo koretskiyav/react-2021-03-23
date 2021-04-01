f5cd433f4d20f272c0395abee6ecdbed97acfc86
ilyazverev1988 <ilya.verez@mail.ru>
2021-04-01 22:38:08 +0300

    ht2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,13 @@
 import React, { PureComponent } from 'react';
 import Restaurants from '../restaurants';
 import Header from '../header';
+import PropTypes from 'prop-types';
 
 export default class App extends PureComponent {
+  static propTypes = {
+    restaurants: PropTypes.arrayOf(PropTypes.object.isRequired).isRequired,
+  };
+
   render() {
     return (
       <div>
--- src/components/rate/rate.js
+++ src/components/rate/rate.js
@@ -2,6 +2,7 @@ import React from 'react';
 import cn from 'classnames';
 
 import { ReactComponent as Star } from '../../icons/star.svg';
+import PropTypes from 'prop-types';
 
 import styles from './rate.module.css';
 
@@ -15,5 +16,9 @@ const Rate = ({ value }) => (
     ))}
   </div>
 );
+
+Rate.propTypes = {
+  value: PropTypes.number,
+};
 
 export default Rate;
