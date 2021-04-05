339e7915c74171dc6dc51989f1262860d4a6e648
Iuliia <u.baukina@gmail.com>
2021-04-05 10:44:39 +0500

    Basket products info

--- src/components/basket/basket.js
+++ src/components/basket/basket.js
@@ -1,43 +1,32 @@
 import styles from './basket.module.css';
-import store from '../../redux/store';
-import { decrement, increment } from '../../redux/actions';
 import { connect } from 'react-redux';
 
-const Basket = ({ basket }) => {
-  // const products = store.getState().order;
-  // const productsArray = [];
-
-  // for (let item in products)
-  // {
-  //   productsArray.push({ id: item.id, name: item.name });
-  // }
-
-  // return <div>
-  //   <h2>Basket: </h2>
-  //   {productsArray.map((product) => {
-  //     <ProductInfo product={product} />
-  //   })}
-  // </div>
-
+const Basket = ({ basket, total }) => {
   const productsArray = [];
 
   for (let key in basket) {
-    productsArray.push(basket[key]);
+    productsArray.push({ product: basket[key], id: key});
   }
 
   return(
     <div>
       <h2>Basket: </h2>
       {productsArray.map((item) => {
-        return <div>{item.name}</div>
+        return <div key={item.id}>
+          <p>{item.product.name}</p>
+          <p>{item.product.amount}</p>
+          <p>{item.product.totalForProduct}</p>
+        </div>
       })}
+      <h4>Total:</h4>
+      <div>{total}</div>
     </div>
   );
 };
 
-const mapStateToProps = (state, props) => ({
-  // amount: state.order[props.product.id] || 0,
-  basket: state.basket
+const mapStateToProps = (state) => ({
+  basket: state.basket,
+  total: state.basket.total
 });
 
 // const mapDispatchToProps = (dispatch, props) => ({
