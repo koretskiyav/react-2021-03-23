1fc0233c7928d0ce3e1f71f141ed0c297cddba49
Irina Nicolai <irina.nic.yu@gmail.com>
2021-04-05 19:43:30 +0200

    Added Basket Component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,12 +2,20 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import Restaurants from '../restaurants';
 import Header from '../header';
+import Basket from '../basket';
 
 export default class App extends PureComponent {
   render() {
+    const allProducts = this.props.restaurants
+      .map((restaurant) => {
+        return restaurant.menu;
+      })
+      .flat();
+
     return (
       <div>
         <Header />
+        <Basket allProducts={allProducts} />
         <Restaurants restaurants={this.props.restaurants} />
       </div>
     );
