22a0ef26ca30128c3ad5ca9aea8a442325973659
Valentina Kirsanova <kirsanova.va@mobilcard.ru>
2021-04-08 17:01:27 +0300

    restaurant and review reducers to normalized data

--- src/redux/reducer/restaurants.js
+++ src/redux/reducer/restaurants.js
@@ -1,4 +1,8 @@
-import { normalizedRestaurants as defaultRestaurants } from '../../fixtures';
+import { normalizedRestaurants } from '../../fixtures';
+
+const defaultRestaurants = normalizedRestaurants
+    .reduce(( obj, restaurant) => { ...obj, [restaurant.id]: restaurant},
+        {});
 
 export default (restaurants = defaultRestaurants, action) => {
   const { type } = action;
--- src/redux/reducer/reviews.js
+++ src/redux/reducer/reviews.js
@@ -1,4 +1,8 @@
-import { normalizedReviews as defaultReviews } from '../../fixtures';
+import { normalizedReviews } from '../../fixtures';
+
+const defaultReviews = normalizedReviews
+    .reduce((obj, review) => { ...obj, [review.id]: review},
+        {});
 
 export default (reviews = defaultReviews, action) => {
   const { type } = action;
