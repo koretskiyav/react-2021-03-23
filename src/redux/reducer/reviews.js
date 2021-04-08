2ded70ca8051cd24e400572c886832cfdf937a5c
Valentina Kirsanova <kirsanova.va@mobilcard.ru>
2021-04-08 22:45:14 +0300

    store for adding review and user

--- src/redux/reducer/reviews.js
+++ src/redux/reducer/reviews.js
@@ -1,13 +1,20 @@
 import { normalizedReviews } from '../../fixtures';
+import { ADD_REVIEW } from '../constants';
 
 const defaultReviews = normalizedReviews
     .reduce((obj, review) => ({ ...obj, [review.id]: review}),
         {});
 
 export default (reviews = defaultReviews, action) => {
-  const { type } = action;
+  const {
+    type,
+    payload
+  } = action;
 
   switch (type) {
+    case ADD_REVIEW:
+      const { id } = payload;
+      return { ...reviews, [id]: payload }
     default:
       return reviews;
   }
--- src/redux/reducer/users.js
+++ src/redux/reducer/users.js
@@ -1,13 +1,19 @@
 import { normalizedUsers } from '../../fixtures';
+import { ADD_USER } from '../constants';
 
 const defaultUsers = normalizedUsers
   .reduce(( obj, user) => ({ ...obj, [user.id]: user}),
     {});
 
 export default (users = defaultUsers, action) => {
-  const { type } = action;
+  const {
+    type,
+    payload
+  } = action;
 
   switch(type) {
+    case ADD_USER:
+      return { ...users, payload: { id: payload} };
     default:
       return users;
   }
