a4436df48e377912eb134267fcb7115985a259e4
koretskiyav <koretskiyav@gmail.com>
2021-04-09 21:12:26 +0300

    add loading state

--- src/redux/reducer/restaurants.js
+++ src/redux/reducer/restaurants.js
@@ -1,16 +1,47 @@
 import produce from 'immer';
-import { ADD_REVIEW, LOAD_RESTAURANTS } from '../constants';
+import {
+  ADD_REVIEW,
+  LOAD_RESTAURANTS,
+  REQUEST,
+  SUCCESS,
+  FAILURE,
+} from '../constants';
 import { arrToMap } from '../utils';
 
-export default (state = {}, action) => {
-  const { type, restaurantId, reviewId, data } = action;
+const initialState = {
+  entities: {},
+  loading: false,
+  loaded: false,
+  error: null,
+};
+
+export default (state = initialState, action) => {
+  const { type, restaurantId, reviewId, data, error } = action;
 
   switch (type) {
-    case LOAD_RESTAURANTS:
-      return arrToMap(data);
+    case LOAD_RESTAURANTS + REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case LOAD_RESTAURANTS + SUCCESS:
+      return {
+        ...state,
+        entities: arrToMap(data),
+        loading: false,
+        loaded: true,
+      };
+    case LOAD_RESTAURANTS + FAILURE:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        error,
+      };
     case ADD_REVIEW:
       return produce(state, (draft) => {
-        draft[restaurantId].reviews.push(reviewId);
+        draft.entities[restaurantId].reviews.push(reviewId);
       });
     default:
       return state;
